57845e9a4f89995dd010acbfc0208fea06b861e8
VICTOR-f1 <ffffvitek@gmail.com>
2022-10-30 23:38:21 +0500

    Initial commit

---
+++ app/src/main/java/com/example/trylistjson/models/GetUUidClass.kt
@@ -0,0 +1,22 @@
+package com.example.trylistjson.models
+
+import androidx.room.Room
+import com.example.trylistjson.AddbookActivity
+import com.example.trylistjson.Bookdatabase
+import com.example.trylistjson.DATABASE_NAME
+import java.util.*
+import java.util.concurrent.Executors
+
+class GetUUidClass {
+//    var db: Bookdatabase = Room.databaseBuilder(    AddbookActivity, Bookdatabase::class.java, DATABASE_NAME ).build()
+//    val bookDAO=db.bookDao()
+//    var adsda:String =""
+//    fun  getuuid (uid:UUID):String{
+//        val executor2 = Executors.newSingleThreadExecutor()
+//        executor2.execute {
+//            adsda=             bookDAO.getString(uid)
+//
+//        }
+//        return adsda
+//    }
+}
